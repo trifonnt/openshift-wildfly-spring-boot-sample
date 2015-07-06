bc22a52d97fc3ab469b0c026c1fe7f5adf6298fd
Eric Murphy <eric.murphy@edlogics.com>
2015-07-05 21:01:46 -0400

    Add local profile

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,9 +1,23 @@
 package demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+		return factory;
+	}
+
+	public static void main( String[] args ) {
+		ApplicationContext ctx = SpringApplication.run( Application.class, args );
+	}
 }
